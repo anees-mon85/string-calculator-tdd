e7288a8b8263a5a6d697d0b639aee0c2dc83afb5
anees-mon85 <aneesmonthayyan@gmail.com>
2025-01-19 15:25:38 +0530

    Update implementation for delimiters of any length

--- string_calculator.py
+++ string_calculator.py
@@ -5,7 +5,11 @@ def add(numbers: str) -> int:
     if not numbers:
         return 0
     if numbers.startswith("//"):
-        delimiter, numbers = re.match(r"//(.+)\n(.*)", numbers).groups()
+        match = re.match(r"//\[(.+)\]\n(.*)", numbers)
+        if match:
+            delimiter, numbers = match.groups()
+        else:
+            delimiter, numbers = re.match(r"//(.+)\n(.*)", numbers).groups()
         numbers = numbers.replace(delimiter, ",")
     numbers = numbers.replace("\n", ",")
     nums = list(map(int, numbers.split(",")))
