ff1631f238f0d0de1de0ae3b691f1f74e2327e89
anees-mon85 <aneesmonthayyan@gmail.com>
2025-01-27 18:32:49 +0530

    Refactor the code by introducing decorator

--- string_calculator.py
+++ string_calculator.py
@@ -1,27 +1,33 @@
 import re
 
 
-def extract_numbers(numbers: str) -> list[int]:
-    if numbers.startswith("//"):
-        match = re.match(r"//(\[.*?\])\n(.*)", numbers)
-        if match:
-            delimiters, numbers = match.groups()
-            delimiters = re.findall(r"\[(.*?)\]", delimiters)
-            for delimiter in delimiters:
+def extract_numbers(func):
+    def wrapper(numbers):
+        if not numbers:
+            return func([])
+        if numbers.startswith("//"):
+            match = re.match(r"//(\[.*?\])\n(.*)", numbers)
+            if match:
+                delimiters, numbers = match.groups()
+                delimiters = re.findall(r"\[(.*?)\]", delimiters)
+                for delimiter in delimiters:
+                    numbers = numbers.replace(delimiter, ",")
+            else:
+                delimiter, numbers = re.match(r"//(.+)\n(.*)", numbers).groups()
                 numbers = numbers.replace(delimiter, ",")
-        else:
-            delimiter, numbers = re.match(r"//(.+)\n(.*)", numbers).groups()
-            numbers = numbers.replace(delimiter, ",")
-    numbers = numbers.replace("\n", ",")
-    return [int(n) for n in numbers.split(",")]
+        numbers = numbers.replace("\n", ",")
+        nums = [int(n) for n in numbers.split(",")]
+        return func(nums)
+
+    return wrapper
 
 
+@extract_numbers
 def add(numbers: str) -> int:
     if not numbers:
         return 0
 
-    nums = extract_numbers(numbers)
-    negatives = [n for n in nums if n < 0]
+    negatives = [n for n in numbers if n < 0]
     if negatives:
         raise ValueError(f"negative numbers not allowed: {','.join(map(str, negatives))}")
-    return sum(n for n in nums if n <= 1000)
+    return sum(n for n in numbers if n <= 1000)
